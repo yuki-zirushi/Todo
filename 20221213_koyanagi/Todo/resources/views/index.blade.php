1d60a1bfc725d855cb4e537ba588154b0ab80831
yuki-zirushi <yuko.12.151@gmail.com>
2022-12-13 15:11:14 +0900

    [Update Css]

--- 20221213_koyanagi/Todo/resources/views/index.blade.php
+++ 20221213_koyanagi/Todo/resources/views/index.blade.php
@@ -6,9 +6,31 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Todo</title>
+  <link rel="stylesheet" href="/css/app.css">
+  <link rel="stylesheet" href="/css/reset.css">
 </head>
 
 <body>
-  <h1>{{$content}}</h1>
+  <div class="container">
+    <div class="card">
+      <p class="title mb-15">Todo List</p>
+      <div class="todo">
+        <form action="#" method="post" class="flex between mb-30">
+          <input type="text" class="input-add" name="content">
+          <input type="submit" class="button-add" value="追加">
+        </form>
+        <table>
+          <tbody>
+            <tr>
+              <th>作成日</th>
+              <th>タスク名</th>
+              <th>更新</th>
+              <th>削除</th>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
 </body>
 </html>
